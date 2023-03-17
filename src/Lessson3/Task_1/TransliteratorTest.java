6605421c554c50ad40e8a76a4e5c2f9f9b9c66d9
kreved77 <81299409+kreved77@users.noreply.github.com>
2023-03-18 00:59:28 +0300

    ADD Lesson3.Task_1: Transliterator

---
+++ src/Lessson3/Task_1/TransliteratorTest.java
@@ -0,0 +1,19 @@
+package Lessson3.Task_1;
+
+public class TransliteratorTest {
+    public static void main(String[] args) {
+        Transliterator transliterator = new TransliteratorImpl();
+
+        String res = transliterator
+                .transliterate("HELLO! ПРИВЕТ! Go, boy!");  // HELLO! PRIVET! Go, boy!
+        System.out.println(res);
+
+        System.out.println(transliterator.transliterate(""));   // ""
+
+        res = transliterator.transliterate("123! АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ! $#, {_+=}!");
+        System.out.println(res);         // 123! ABVGDEEZHZIIKLMNOPRSTUFKHTSCHSHSHCHIEYEIUIA! $#, {_+=}!
+
+        System.out.println(transliterator.transliterate("СЛУЧАЙНО НАЖАЛИ \"caps lock\" И ВВЕЛИ ТЕКСТ, НЕ ПРОБЛЕМА!"));
+                                                      // SLUCHAINO NAZHALI "caps lock" I VVELI TEKST, NE PROBLEMA!
+    }
+}
