673405feabd9eced540c18717512878a80d25a99
kreved77 <81299409+kreved77@users.noreply.github.com>
2023-03-21 00:53:08 +0300

    upd Task_5: FileSort - dynamic chunkSize detection

--- src/Lesson3/Task_5_FileSort_ExternalMergeSort/SorterTest.java
+++ src/Lesson3/Task_5_FileSort_ExternalMergeSort/SorterTest.java
@@ -12,17 +12,21 @@ public class SorterTest {
 
 //        File dataFile = new Generator().generate("src/Lesson3/Task_5_FileSort_ExternalMergeSort/data/data.txt", 375_000_000);
         File dataFile = new Generator().generate("src/Lesson3/Task_5_FileSort_ExternalMergeSort/data/data.txt", 1_000);
+        System.out.println("Data file is generated.");
         System.out.println(new Validator(dataFile).isSorted()); // false
-//        filePrinter(dataFile);    // for small files (<20) check
 
 
         File sortedFile = new Sorter().sortFile(dataFile);
-        System.out.println((System.currentTimeMillis() - beginTime) / 1000 + " (sec) = time of execution");
+        System.out.println((System.currentTimeMillis() - beginTime) / 1000 + " (sec) = time for sortFile \nWaiting for check:");
         System.out.println(new Validator(sortedFile).isSorted()); // true
+
+
+
+//        filePrinter(dataFile);    // for small files (<20) check
 //        filePrinter(sortedFile);    // for small files (<20) check
 
 
-        // delete files when the program terminates
+        // delete files when Process finished
 //        dataFile.deleteOnExit();
 //        sortedFile.deleteOnExit();
     }
