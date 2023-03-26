8b294f1846b8fcba8b92c869449d443a5928ba46
kreved77 <81299409+kreved77@users.noreply.github.com>
2023-03-26 19:02:50 +0300

    ADD Lesson4.Task_2: PersistentMap

--- src/io/ylab/intensive/lesson04/persistentmap/PersistenceMapTest.java
+++ src/io/ylab/intensive/lesson04/persistentmap/PersistenceMapTest.java
@@ -9,12 +9,46 @@ public class PersistenceMapTest {
   public static void main(String[] args) throws SQLException {
     DataSource dataSource = initDb();
     PersistentMap persistentMap = new PersistentMapImpl(dataSource);
-    // Написать код демонстрации работы 
+
+    mapGenerate(persistentMap);   // optional generator: Map_1, Map_2, Map_3
+                                  // with "key_9"-"value_9", "key_10"-"value_10", "key_11"-"value_11"
+
+    persistentMap.init("Map_1");
+    System.out.println(persistentMap.containsKey("key_10"));  // true
+    persistentMap.remove("key_10");                       // after remove
+    System.out.println(persistentMap.containsKey("key_10"));  // false
+
+    for (String s : persistentMap.getKeys()) {
+      System.out.println("RETURN getKeys = " + s);            // key_9, key_11
+    }
+
+    System.out.println("==========");
+    persistentMap.init("Map_3");
+    System.out.println("Map_3 initial value : key_11 -> " + persistentMap.get("key_11"));
+    persistentMap.put("key_11", "value_5555");                // updated value
+    System.out.println("Map_3 updated value : key_11 -> " + persistentMap.get("key_11"));
+
+    System.out.println("==========");
+    persistentMap.init("Map_2");                        // key_9, key_10, key_11
+    persistentMap.clear();
+    for (String s : persistentMap.getKeys()) {
+      System.out.println("RETURN getKeys = " + s);            // none
+    }
   }
-  
+
+  private static void mapGenerate(PersistentMap persistentMap) throws SQLException {
+    int mapCount = 0;
+    while (mapCount++ < 3) {
+      persistentMap.init("Map_" + mapCount);
+      for (int i = 9; i < 12; i++) {
+        persistentMap.put("key_" + i, "value_" + i);
+      }
+    }
+  }
+
   public static DataSource initDb() throws SQLException {
     String createMapTable = "" 
-                                + "drop table if exists persistent_map; " 
+                                + "drop table if exists persistent_map; "
                                 + "CREATE TABLE if not exists persistent_map (\n"
                                 + "   map_name varchar,\n"
                                 + "   KEY varchar,\n"
--- src/io/ylab/intensive/lesson04/persistentmap/PersistentMapImpl.java
+++ src/io/ylab/intensive/lesson04/persistentmap/PersistentMapImpl.java
@@ -1,6 +1,10 @@
 package io.ylab.intensive.lesson04.persistentmap;
 
 import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 import javax.sql.DataSource;
 
@@ -8,7 +12,8 @@ import javax.sql.DataSource;
  * Класс, методы которого надо реализовать 
  */
 public class PersistentMapImpl implements PersistentMap {
-  
+
+  private String mapName;
   private DataSource dataSource;
 
   public PersistentMapImpl(DataSource dataSource) {
@@ -17,36 +22,88 @@ public class PersistentMapImpl implements PersistentMap {
 
   @Override
   public void init(String name) {
-    
+    this.mapName = name;
   }
 
   @Override
   public boolean containsKey(String key) throws SQLException {
-    return false;
+    String containsKeyQuery = "SELECT key FROM persistent_map WHERE map_name=? AND key=?";
+    try (Connection connection = dataSource.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement(containsKeyQuery)) {
+      preparedStatement.setString(1, mapName);
+      preparedStatement.setString(2, key);
+      return preparedStatement.executeQuery().next();
+    }
   }
 
   @Override
   public List<String> getKeys() throws SQLException {
-    return null;
+    List<String> listKeys = new ArrayList<>();
+    String getKeysQuery = "SELECT key FROM persistent_map WHERE map_name=?";
+    try (Connection connection = dataSource.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement(getKeysQuery)) {
+      preparedStatement.setString(1, mapName);
+      ResultSet resultSet = preparedStatement.executeQuery();
+
+      while (resultSet.next()) {
+        String str = resultSet.getString(1);
+        listKeys.add(str);
+      }
+    }
+    return listKeys;
   }
 
   @Override
   public String get(String key) throws SQLException {
-    return null;
+    String getValueQuery = "SELECT value FROM persistent_map WHERE map_name=? AND key=?";
+    try (Connection connection = dataSource.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement(getValueQuery)) {
+      preparedStatement.setString(1, mapName);
+      preparedStatement.setString(2, key);
+
+      ResultSet resultSet = preparedStatement.executeQuery();
+      return (resultSet.next()) ? resultSet.getString(1) : "";
+    }
   }
 
   @Override
   public void remove(String key) throws SQLException {
+    String removeQuery = "DELETE FROM persistent_map WHERE map_name=? AND key=?";
+    try (Connection connection = dataSource.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement(removeQuery)) {
+      preparedStatement.setString(1,mapName);
+      preparedStatement.setString(2,key);
 
+      int i = preparedStatement.executeUpdate();
+      System.out.println(i + " record deleted");
+    }
   }
 
   @Override
   public void put(String key, String value) throws SQLException {
-
+    if (containsKey(key)) {
+      remove(key);
+    }
+    String insertQuery = "insert into persistent_map (map_name, KEY, value) values (?, ?, ?)";
+    try (Connection connection = dataSource.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+      preparedStatement.setString(1, mapName);
+      preparedStatement.setString(2, key);
+      preparedStatement.setString(3, value);
+      preparedStatement.executeUpdate();
+    }
   }
 
   @Override
   public void clear() throws SQLException {
+    String clearQuery = "DELETE FROM persistent_map WHERE map_name=?";
+    try (Connection connection = dataSource.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement(clearQuery)) {
+      preparedStatement.setString(1,mapName);
 
+      int i = preparedStatement.executeUpdate();
+      System.out.println(i + " records deleted. Cleared mapName=" + mapName);
+    }
   }
+
 }
