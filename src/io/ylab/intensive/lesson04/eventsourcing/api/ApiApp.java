3010a02a4670fc5c3cf5aa29d3f8f3f9127f649d
kreved77 <81299409+kreved77@users.noreply.github.com>
2023-03-27 22:12:43 +0300

    config Lesson4.Task_4: EventSourcing - setup RabbitMQ, test connection and queue bind (message publish/receive)

--- src/io/ylab/intensive/lesson04/eventsourcing/api/ApiApp.java
+++ src/io/ylab/intensive/lesson04/eventsourcing/api/ApiApp.java
@@ -1,5 +1,8 @@
 package io.ylab.intensive.lesson04.eventsourcing.api;
 
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import io.ylab.intensive.lesson04.RabbitMQUtil;
 
@@ -7,6 +10,18 @@ public class ApiApp {
   public static void main(String[] args) throws Exception {
     ConnectionFactory connectionFactory = initMQ();
     // Тут пишем создание PersonApi, запуск и демонстрацию работы
+
+    String exchangeName = "exc";
+    String queueName = "queue";
+    try (Connection connection = connectionFactory.newConnection();
+         Channel channel = connection.createChannel()) {
+      channel.exchangeDeclare(exchangeName, BuiltinExchangeType.TOPIC);
+      channel.queueDeclare(queueName, true, false, false, null);
+      channel.queueBind(queueName, exchangeName, "*");
+
+      channel.basicPublish(exchangeName, "*", null, "Hello World".getBytes());
+//      channel.basicPublish(exchangeName, queueName, null, "Hello World".getBytes());
+    }
   }
 
   private static ConnectionFactory initMQ() throws Exception {
--- src/io/ylab/intensive/lesson04/eventsourcing/db/DbApp.java
+++ src/io/ylab/intensive/lesson04/eventsourcing/db/DbApp.java
@@ -3,7 +3,10 @@ package io.ylab.intensive.lesson04.eventsourcing.db;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.GetResponse;
 import io.ylab.intensive.lesson04.DbUtil;
 import io.ylab.intensive.lesson04.RabbitMQUtil;
 
@@ -13,6 +16,26 @@ public class DbApp {
     ConnectionFactory connectionFactory = initMQ();
 
     // тут пишем создание и запуск приложения работы с БД
+    String queueName = "queue";
+    try (Connection connection = connectionFactory.newConnection();
+         Channel channel = connection.createChannel()) {
+      while (!Thread.currentThread().isInterrupted()) {
+        GetResponse message = channel.basicGet(queueName, true);
+
+        try {
+          Thread.sleep(1023);
+        } catch (InterruptedException e) {
+          throw new RuntimeException(e);
+        }
+        System.out.println(System.currentTimeMillis());
+        if (message == null) {
+
+        } else {
+          String received = new String(message.getBody());
+          System.out.println(received);
+        }
+      }
+    }
   }
   
   private static ConnectionFactory initMQ() throws Exception {
@@ -20,9 +43,9 @@ public class DbApp {
   }
   
   private static DataSource initDb() throws SQLException {
-    String ddl = "" 
+    String ddl = ""
                      + "drop table if exists person;" 
-                     + "create if not exists table person (\n"
+                     + "CREATE TABLE if not exists person (\n"
                      + "person_id bigint primary key,\n"
                      + "first_name varchar,\n"
                      + "last_name varchar,\n"
