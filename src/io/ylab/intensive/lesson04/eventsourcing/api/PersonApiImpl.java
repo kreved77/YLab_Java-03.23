fda1c36787c5dbd4579d1fbeadc9a7f30eaed814
kreved77 <81299409+kreved77@users.noreply.github.com>
2023-03-28 17:00:31 +0300

    ADD Lesson4.Task_4: EventSourcing

--- src/io/ylab/intensive/lesson04/eventsourcing/api/PersonApiImpl.java
+++ src/io/ylab/intensive/lesson04/eventsourcing/api/PersonApiImpl.java
@@ -1,30 +1,173 @@
 package io.ylab.intensive.lesson04.eventsourcing.api;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import io.ylab.intensive.lesson04.eventsourcing.Person;
+
+import javax.sql.DataSource;
 
 /**
  * Тут пишем реализацию
  */
 public class PersonApiImpl implements PersonApi {
+  private DataSource dataSource;
+
+  public PersonApiImpl() {
+//    try {
+//      this.dataSource = initDb();
+//    } catch (SQLException e) {
+//      throw new RuntimeException(e);
+//    }
+  }
+  public PersonApiImpl(DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
+
   @Override
   public void deletePerson(Long personId) {
-    
+    System.out.println(deletePersonQueueMessageGenerate(personId));
+//    deletePersonQueueMessageGenerate(personId);
+  }
+
+  public String deletePersonQueueMessageGenerate(Long personId) {
+    try {
+      ApiApp.sendMessage("del;" + personId);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    return "deletePerson queue message generated";
   }
 
   @Override
   public void savePerson(Long personId, String firstName, String lastName, String middleName) {
+    System.out.println(savePersonQueueMessageGenerate(personId, firstName, lastName, middleName));
+//    savePersonQueueMessageGenerate(personId, firstName, lastName, middleName);
+  }
 
+  public String savePersonQueueMessageGenerate(Long personId, String firstName, String lastName, String middleName) {
+    try {
+      ApiApp.sendMessage("sav;" + personId + ";" + firstName + ";" + lastName + ";" + middleName + "; ");
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    return "savePerson queue message generated";
   }
 
   @Override
   public Person findPerson(Long personId) {
+    String findPersonQuery = "SELECT * FROM person WHERE person_id=?";
+    try (Connection connection = dataSource.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement(findPersonQuery)) {
+      preparedStatement.setLong(1, personId);
+      ResultSet resultSet = preparedStatement.executeQuery();
+
+      if (resultSet.next()) {
+        Person person = new Person();
+        person.setId(personId);
+        person.setName(resultSet.getString(2));
+        person.setLastName(resultSet.getString(3));
+        person.setMiddleName(resultSet.getString(4));
+        return person;
+      } else {
+        System.out.println("Check personId=" + personId + " : record not found");
+      }
+    } catch (SQLException e) {
+      System.err.println(e);
+    }
     return null;
   }
 
   @Override
   public List<Person> findAll() {
-    return null;
+    List<Person> personList = new ArrayList<>();
+    String findAllQuery = "SELECT * FROM person ORDER BY person_id ASC";
+    try (Connection connection = dataSource.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement(findAllQuery)) {
+      ResultSet resultSet = preparedStatement.executeQuery();
+
+      while (resultSet.next()) {
+        Person person = new Person();
+        person.setId(Long.valueOf(resultSet.getString(1)));
+        person.setName(resultSet.getString(2));
+        person.setLastName(resultSet.getString(3));
+        person.setMiddleName(resultSet.getString(4));
+        personList.add(person);
+      }
+    } catch (SQLException e) {
+      System.err.println(e);
+    }
+    return personList;
   }
+
+
+//  public void deletePerson(Long personId, DataSource dataSource) {
+////    if (dataSource == null) {
+////      System.out.println(deletePersonQueueMessageGenerate(personId));
+//////      deletePersonQueueMessageGenerate(personId);
+////    } else {
+//      if (findPerson(personId) == null) {
+//        System.err.println("Warning (at 'deletePerson')! No Person with personId=" + personId);
+//      } else {
+//        String removeQuery = "DELETE FROM person WHERE person_id=?";
+//        try (Connection connection = dataSource.getConnection();
+//             PreparedStatement preparedStatement = connection.prepareStatement(removeQuery)) {
+//          preparedStatement.setLong(1, personId);
+//          preparedStatement.executeUpdate();
+//          System.out.println("Person with personId=" + personId + " record deleted");
+//        } catch (SQLException e) {
+//          System.err.println(e);
+//        }
+//      }
+////    }
+//  }
+
+//  @Override
+//  public void savePerson(Long personId, String firstName, String lastName, String middleName) {
+//    if (dataSource == null) {
+//      System.out.println(savePersonQueueMessageGenerate(personId, firstName, lastName, middleName));
+//      savePersonQueueMessageGenerate(personId, firstName, lastName, middleName);
+//    } else {
+//      Person person = findPerson(personId);
+//      if (person == null) {
+//        String insertQuery = "insert into person (person_id, first_name, last_name, middle_name) values (?, ?, ?, ?)";
+//        try (Connection connection = dataSource.getConnection();
+//             PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+//          preparedStatement.setLong(1, personId);
+//          preparedStatement.setString(2, firstName);
+//          preparedStatement.setString(3, lastName);
+//          preparedStatement.setString(4, middleName);
+//          preparedStatement.executeUpdate();
+//        } catch (NullPointerException nullPointerException) {
+//          System.err.println("Warning (at 'savePerson')! Some Person data is null.");
+//        } catch (SQLException e) {
+//          System.err.println(e);
+//        }
+//      } else {
+//        updatePerson(person, personId, firstName, lastName, middleName);
+//      }
+//    }
+//  }
+
+//  private void updatePerson(Person person, Long personId, String firstName, String lastName, String middleName) {
+//    String updateQuery = "UPDATE person SET first_name=?, last_name=?, middle_name=? WHERE person_id=?";
+//    try (Connection connection = dataSource.getConnection();
+//         PreparedStatement preparedStatement = connection.prepareStatement(updateQuery)) {
+//      preparedStatement.setString(1, firstName.equals("") ? person.getName() : firstName);
+//      preparedStatement.setString(2, lastName.equals("") ? person.getLastName() : lastName);
+//      preparedStatement.setString(3, middleName.equals("") ? person.getMiddleName() : middleName);
+//      preparedStatement.setLong(4, personId);
+//      preparedStatement.executeUpdate();
+//    } catch (NullPointerException nullPointerException) {
+//      System.err.println("Warning (at 'savePerson -> updatePerson')! Some Person data is null.");
+//    } catch (SQLException e) {
+//      System.err.println(e);
+//    }
+//  }
+
+//  public static DataSource initDb() throws SQLException {
+//    DataSource dataSource = DbUtil.buildDataSource();
+//    return dataSource;
+//  }
 }
--- src/io/ylab/intensive/lesson04/eventsourcing/db/DbApp.java
+++ src/io/ylab/intensive/lesson04/eventsourcing/db/DbApp.java
@@ -1,14 +1,15 @@
 package io.ylab.intensive.lesson04.eventsourcing.db;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.GetResponse;
+import com.rabbitmq.client.*;
 import io.ylab.intensive.lesson04.DbUtil;
 import io.ylab.intensive.lesson04.RabbitMQUtil;
+import io.ylab.intensive.lesson04.eventsourcing.Person;
+import io.ylab.intensive.lesson04.eventsourcing.api.PersonApi;
+import io.ylab.intensive.lesson04.eventsourcing.api.PersonApiImpl;
 
 public class DbApp {
   public static void main(String[] args) throws Exception {
@@ -22,29 +23,101 @@ public class DbApp {
       while (!Thread.currentThread().isInterrupted()) {
         GetResponse message = channel.basicGet(queueName, true);
 
-        try {
-          Thread.sleep(1023);
-        } catch (InterruptedException e) {
-          throw new RuntimeException(e);
-        }
-        System.out.println(System.currentTimeMillis());
-        if (message == null) {
-
-        } else {
+        if (message != null) {
           String received = new String(message.getBody());
-          System.out.println(received);
+          parseMessage(received, dataSource);
         }
       }
     }
   }
-  
+
+  private static void parseMessage(String received, DataSource dataSource) {
+    System.out.println("\n NEXT: " + received);
+    if ("sav".equals(received.substring(0,3))) {
+      String[] strArr = received.substring(4).split(";");
+//      PersonApi personApi = new PersonApiImpl(dataSource);
+//      personApi.savePerson(Long.valueOf(strArr[0]), strArr[1], strArr[2], strArr[3]);
+      savePerson(Long.valueOf(strArr[0]), strArr[1], strArr[2], strArr[3], dataSource);
+    }
+    if ("del".equals(received.substring(0,3))) {
+      String str = received.substring(4);
+//      PersonApi personApi = new PersonApiImpl(dataSource);
+//      personApi.deletePerson(Long.valueOf(str));
+      deletePerson(Long.valueOf(str), dataSource);
+    }
+  }
+
+
+  private static void deletePerson(Long personId, DataSource dataSource) {
+    String removeQuery = "DELETE FROM person WHERE person_id=?";
+    try (java.sql.Connection connection = dataSource.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement(removeQuery)) {
+      preparedStatement.setLong(1, personId);
+
+      if (preparedStatement.executeUpdate() == 1) {
+        System.out.println("Person with personId=" + personId + " record deleted");
+      } else {
+        System.err.println("Warning (at 'deletePerson')! No Person with personId=" + personId);
+      }
+    } catch (SQLException e) {
+      System.err.println(e);
+    }
+  }
+
+  private static void savePerson(Long personId, String firstName, String lastName, String middleName, DataSource dataSource) {
+    PersonApi personApi = new PersonApiImpl(dataSource);
+    Person person = personApi.findPerson(personId);
+    if (person == null) {
+      String insertQuery = "insert into person (person_id, first_name, last_name, middle_name) values (?, ?, ?, ?)";
+      try (java.sql.Connection connection = dataSource.getConnection();
+           PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+        preparedStatement.setLong(1, personId);
+        preparedStatement.setString(2, firstName);
+        preparedStatement.setString(3, lastName);
+        preparedStatement.setString(4, middleName);
+
+        if (preparedStatement.executeUpdate() == 1) {
+          System.out.println("Person with personId=" + personId + " record saved");
+        } else {
+          System.err.println("Warning (at 'savePerson')! For Person with personId=" + personId);
+        }
+//      } catch (NullPointerException nullPointerException) {
+//        System.err.println("Warning (at 'savePerson')! Some Person data is null.");
+      } catch (SQLException e) {
+        System.err.println(e);
+      }
+    } else {
+      updatePerson(person, personId, firstName, lastName, middleName, dataSource);
+    }
+  }
+
+
+  private static void updatePerson(Person person, Long personId, String firstName, String lastName, String middleName, DataSource dataSource) {
+    String updateQuery = "UPDATE person SET first_name=?, last_name=?, middle_name=? WHERE person_id=?";
+    try (java.sql.Connection connection = dataSource.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement(updateQuery)) {
+      preparedStatement.setString(1, firstName.equals("") ? person.getName() : firstName);
+      preparedStatement.setString(2, lastName.equals("") ? person.getLastName() : lastName);
+      preparedStatement.setString(3, middleName.equals("") ? person.getMiddleName() : middleName);
+      preparedStatement.setLong(4, personId);
+
+      if (preparedStatement.executeUpdate() == 1) {
+        System.out.println("Person with personId=" + personId + " record updated");
+      } else {
+        System.err.println("Warning (at 'savePerson - updatePerson')! For Person with personId=" + personId);
+      }
+    } catch (SQLException e) {
+      System.err.println(e);
+    }
+  }
+
   private static ConnectionFactory initMQ() throws Exception {
     return RabbitMQUtil.buildConnectionFactory();
   }
   
   private static DataSource initDb() throws SQLException {
     String ddl = ""
-                     + "drop table if exists person;" 
+                     + "drop table if exists person;"
                      + "CREATE TABLE if not exists person (\n"
                      + "person_id bigint primary key,\n"
                      + "first_name varchar,\n"
