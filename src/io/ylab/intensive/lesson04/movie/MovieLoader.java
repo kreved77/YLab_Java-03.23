4994aa106f658cadceb65bc4626689352dadb983
kreved77 <81299409+kreved77@users.noreply.github.com>
2023-03-29 12:02:54 +0300

    upd Lesson4.Task_1: MovieLoader - group Exceptions

--- src/io/ylab/intensive/lesson04/movie/MovieLoader.java
+++ src/io/ylab/intensive/lesson04/movie/MovieLoader.java
@@ -1,8 +1,7 @@
 package io.ylab.intensive.lesson04.movie;
 
 import java.io.File;
-import java.io.IOException;
 
 public interface MovieLoader {
-  void loadData(File file) throws IOException;
+  void loadData(File file);
 }
