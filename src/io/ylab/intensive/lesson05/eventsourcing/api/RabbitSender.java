9227337467f77ccae8fb888550bd85763a6e7812
kreved77 <81299409+kreved77@users.noreply.github.com>
2023-04-02 18:52:02 +0300

    fix Lesson5.Task_1: EventSourcing - add to RabbitSender: createExchangeQueueBind()

--- src/io/ylab/intensive/lesson05/eventsourcing/api/RabbitSender.java
+++ src/io/ylab/intensive/lesson05/eventsourcing/api/RabbitSender.java
@@ -1,10 +1,14 @@
 package io.ylab.intensive.lesson05.eventsourcing.api;
 
+import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 @Component
 public class RabbitSender {
@@ -15,12 +19,26 @@ public class RabbitSender {
     @Autowired
     public RabbitSender(ConnectionFactory connectionFactory) {
         this.connectionFactory = connectionFactory;
+        createExchangeQueueBind();
     }
     public static void sendMessageToQueue(String message) throws Exception {
         try (Connection connection = connectionFactory.newConnection();
              Channel channel = connection.createChannel()) {
 
             channel.basicPublish(exchangeName, queueName, null, message.getBytes());
+        }
+    }
+
+    public static void createExchangeQueueBind() {
+        try (Connection connection = connectionFactory.newConnection();   // createExchangeQueueBind
+             Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(exchangeName, BuiltinExchangeType.TOPIC);
+            channel.queueDeclare(queueName, true, false, false, null);
+            channel.queueBind(queueName, exchangeName, "*");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (TimeoutException e) {
+            throw new RuntimeException(e);
         }
     }
 }
